59fd57207b22d65d5e0012970e9320d16bb81cfb
Rob Wiebe <rjw.ywam@gmail.com>
2018-11-15 20:48:37 -1000

    missed unsaved file

--- sharebiblesmap/functions/index.js
+++ sharebiblesmap/functions/index.js
@@ -1,10 +1,13 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
+// const firebaseHelper = require('firebase-functions-helper'); 
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
+exports.rob = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
         if (request.method !== 'POST') {
             return response.status(500).json({
